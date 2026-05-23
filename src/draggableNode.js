ded83184275ca24270c23d17080597da4d37216f
Ajay Prajapati <prajapatisani9@gmail.com>
2026-05-23 18:24:51 +0530

    nodes styling after adding images

--- src/draggableNode.js
+++ src/draggableNode.js
@@ -1,4 +1,26 @@
 // draggableNode.js
+import './style/draggableNode.css'
+import inputIcon from './images/input.svg';
+import outputIcon from './images/output.svg';
+import llmIcon from './images/llm.svg';
+import textIcon from './images/text.svg';
+import apiRequest from './images/apiRequest.svg';
+import APIResponse from './images/apiResponse.svg';
+import dbIcon from './images/db.svg';
+import filterIcon from './images/filter.svg';
+import emailIcon from './images/email.svg';
+
+const getIconUrl = (type) => {
+  if (type === 'customInput') return inputIcon;
+  if (type === 'customOutput') return outputIcon;
+  if (type === 'llm') return llmIcon;
+  if (type === 'text') return textIcon;
+  if (type === 'APIResponse') return APIResponse;
+  if (type === 'APIRequest') return apiRequest;
+  if (type === 'FilterNode') return filterIcon;
+  if (type === 'DBNode') return dbIcon;
+  if (type === 'EmailNode') return emailIcon;
+};
 
 export const DraggableNode = ({ type, label }) => {
     const onDragStart = (event, nodeType) => {
@@ -10,23 +32,20 @@ export const DraggableNode = ({ type, label }) => {
   
     return (
       <div
-        className={type}
+        id={type}
         onDragStart={(event) => onDragStart(event, type)}
         onDragEnd={(event) => (event.target.style.cursor = 'grab')}
         style={{ 
-          cursor: 'grab', 
-          minWidth: '80px', 
-          height: '60px',
-          display: 'flex', 
-          alignItems: 'center', 
-          borderRadius: '8px',
-          backgroundColor: '#1C2536',
-          justifyContent: 'center', 
-          flexDirection: 'column'
-        }} 
+          
+        }}
+        className="DraggableNode" 
         draggable
-      >
-          <span style={{ color: '#fff' }}>{label}</span>
+      > <img
+        src={getIconUrl(type)}
+        alt={label}
+        style={{ width: '24px', height: '24px' }}
+      />
+          <span>{label}</span>
       </div>
     );
   };
